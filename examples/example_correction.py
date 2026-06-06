933ac2d4aee72799bcbfdceed8c043aeff549f05
fdrgsp <federico.gasparoli@gmail.com>
2026-06-06 18:10:13 -0400

    examples

--- examples/example_correction.py
+++ examples/example_correction.py
@@ -3,22 +3,20 @@ from rich import print
 import ndv
 from microcal._sample_generator import generate_beads_image
 
-# img = tifffile.imread("bead_image.tiff")
 img, meta = generate_beads_image(
-    n_channels=3,
+    n_channels=2,
     shape=(512, 512),
     n_beads=50,
     bead_sigma=2,
     bead_intensity=60.0,
     bit_depth=16,
     offset=100,
-    shifts=[(0, 0), (1.5, -2.5), (-2.0, 1.0)],
-    rotations=[0, 5, -3],
-    scales=[(1, 1), (1.05, 0.95), (0.95, 1.05)],
+    shifts=[(0, 0), (1.5, -2.5)],
+    rotations=[0, 5],
+    scales=[(1, 1), (1.05, 0.95)],
     snr=8,
-    seed=42
+    seed=42,
 )
-
 ndv.imshow(img)
 
 sc = ChromaticShiftCorrector(
@@ -33,12 +31,10 @@ sc = ChromaticShiftCorrector(
 )
 
 results = sc.measure(img)
-
 print(results)
 
 val = sc.validate()
 print(val)
 
 image_corr = sc.apply(image_or_stack=img, result=results, crop=True)
-
 ndv.imshow(image_corr)
